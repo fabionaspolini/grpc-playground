e1c1fdc8f5c7a8d06450e9727528da23df062138
Fábio Naspolini <fabionaspolini@gmail.com>
2021-12-03 18:05:52 -0300

    Ajustes

--- dotnet-client/Program.cs
+++ dotnet-client/Program.cs
@@ -5,7 +5,7 @@ using Grpc.Net.Client;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine(".:: dotnet GRPC Client ::.");
 
-using var channel = GrpcChannel.ForAddress("http://localhost:5087");
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
 var client = new Greeter.GreeterClient(channel);
 var reply = await client.SayHelloAsync(new HelloRequest() { Name = "Teste GRPC" });
 Console.WriteLine("Retorno: " + reply.Message);
@@ -14,7 +14,3 @@ Console.WriteLine("Retorno: " + reply.Message);
 // Para simular coloque um break point aqui e reinicie o GrpcServer.
 var reply2 = await client.SayHelloAsync(new HelloRequest() { Name = "Teste GRPC 2" });
 Console.WriteLine("Retorno2: " + reply2.Message);
-
-
-// Console.WriteLine("Pressione qualquer tecla para sair.");
-// Console.ReadKey();
